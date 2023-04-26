46940167b1da104733adc704c0dbf065fb72c332
cneumann01 <carterneumann@protonmail.com>
2023-04-26 17:33:55 -0400

    added session/retake survey functionality

--- 24 Flask/24.3 Flask Tools Project/app.py
+++ 24 Flask/24.3 Flask Tools Project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 import surveys as s
 
@@ -15,8 +15,15 @@ responses = []
 def home_page():
     return render_template('home.html', title=sat_survey.title, instructions=sat_survey.instructions)
 
+@app.route('/init')
+def init():
+    responses = []
+    session['responses'] = responses
+    return redirect('/question/0')
+
 @app.route('/question/<int:question_num>')
 def show_question(question_num):
+    responses = session['responses']
     if len(responses) != question_num:
         flash(f'Please answer the questions in order. Thanks!')
         return redirect(f'/question/{len(responses)}')
@@ -24,7 +31,9 @@ def show_question(question_num):
 
 @app.route('/answer', methods=['POST'])
 def answer_question():
+    responses = session['responses']
     responses.append(request.form['answer'])
+    session['responses'] = responses
     if len(responses) == len(sat_survey.questions):
         return redirect('/thankyou')
     else:
