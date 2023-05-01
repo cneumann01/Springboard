6e78b05b8fc7c00c774e6cd25a72cdaa3a95721b
cneumann01 <carterneumann@protonmail.com>
2023-05-01 18:32:53 -0400

    implemted word checking/basic score system

--- 24 Flask/24.5 Flask Testing/app.py
+++ 24 Flask/24.5 Flask Testing/app.py
@@ -1,15 +1,31 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, flash, jsonify, redirect
 
 app = Flask(__name__)
 app.secret_key = 'secret'
 
 boggle_game = Boggle()
 board = boggle_game.make_board()
+words_found = []
+score = 0
 
 @app.route('/')
 def game_board():
     """Display game board."""
+    flash(f"Current Score: {session['score']}")
     session['board'] = board
     return render_template('board.html', board=board)
 
+@app.route('/check-word')
+def check_word():
+    """Check if word is valid."""
+    word = request.args['word'].lower()
+    result = boggle_game.check_valid_word(board, word)
+    if result == 'ok':
+        session['score'] = session.get('score', 0) + len(word)
+        flash(f'Great! You found a word! +{len(word)} points')
+    elif result == 'not-on-board':
+        flash('That word is not on the board.')
+    elif result == 'not-word':
+        flash('That is not a valid word.')
+    return redirect('/')
