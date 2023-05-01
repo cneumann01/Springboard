b44ad91101cfb2f463257dd22a454f50b1662673
cneumann01 <carterneumann@protonmail.com>
2023-05-01 17:26:28 -0400

    basic Li view of the board

--- 24 Flask/24.5 Flask Testing/app.py
+++ 24 Flask/24.5 Flask Testing/app.py
@@ -1,4 +1,15 @@
 from boggle import Boggle
 from flask import Flask, request, render_template, session
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
 boggle_game = Boggle()
+board = boggle_game.make_board()
+
+@app.route('/')
+def game_board():
+    """Display game board."""
+    session['board'] = board
+    return render_template('board.html', board=board)
 
