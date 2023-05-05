6a71c39fbeb30c985008eec051c1b08eab6603b8
cneumann01 <carterneumann@protonmail.com>
2023-05-05 15:07:08 -0400

    working guesses/score system

--- 24 Flask/24.5 Flask Testing/app.py
+++ 24 Flask/24.5 Flask Testing/app.py
@@ -12,18 +12,23 @@ score = 0
 @app.route('/')
 def game_board():
     """Display game board."""
-    flash(f"Current Score: {session['score']}")
+    flash(f"Current Score: {score}")
     session['board'] = board
     return render_template('board.html', board=board)
 
 @app.route('/check-word')
 def check_word():
+    """Allows local access to score variable."""
+    global score
     """Check if word is valid."""
     word = request.args['word'].lower()
     result = boggle_game.check_valid_word(board, word)
-    if result == 'ok':
-        session['score'] = session.get('score', 0) + len(word)
+    if word in words_found:
+        flash('You already found that word!')
+    elif result == 'ok':
+        score += len(word)
         flash(f'Great! You found a word! +{len(word)} points')
+        words_found.append(word)
     elif result == 'not-on-board':
         flash('That word is not on the board.')
     elif result == 'not-word':
