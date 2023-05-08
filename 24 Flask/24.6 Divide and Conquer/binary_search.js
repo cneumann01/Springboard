73ad8d9e8efa95b558d74b24cd1e996662bf40b9
cneumann01 <carterneumann@protonmail.com>
2023-05-08 10:20:51 -0400

    updated auto-formatting

--- 24 Flask/24.6 Divide and Conquer/binary_search.js
+++ 24 Flask/24.6 Divide and Conquer/binary_search.js
@@ -1,18 +1,18 @@
 function binarySearch(array, target) {
-  let left = 0;
-  let right = array.length - 1;
+	let left = 0;
+	let right = array.length - 1;
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2);
 
-    if (array[mid] === target) {
-      return mid;
-    } else if (array[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
+		if (array[mid] === target) {
+			return mid;
+		} else if (array[mid] < target) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
 
-  return -1; // Indicates target was not found in array
+	return -1; // Indicates target was not found in array
 }
