32eaea9dcf9fcdb49c06b26c3458a26c588ab39c
cneumann01 <carterneumann@protonmail.com>
2023-05-17 13:16:04 -0400

    working user database/flask routes

--- 28 SQLAlchemy/28.1 Intro/app.py
+++ 28 SQLAlchemy/28.1 Intro/app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template, request, flash, session
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,4 +10,64 @@ app.config['SECRET_KEY'] = 'secret'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
-# db.create_all()
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    """Redirect to list of users."""
+    return redirect('/users')
+
+@app.route('/users')
+def list_users():
+    """Show all users."""
+    users = User.query.all()
+    return render_template('list.html', users=users)
+
+@app.route('/users/new')
+def new_user_form():
+    """Show an add form for users."""
+    return render_template('new.html')
+
+@app.route('/users/new', methods=['POST'])
+def add_user():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    new_user = User(first_name=first_name,last_name=last_name,image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>')
+def show_user(user_id):
+    """Show information about the given user."""
+    user = User.query.get_or_404(user_id)
+    return render_template('show.html', user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    """Show edit form for a user."""
+    user = User.query.get_or_404(user_id)
+    return render_template('edit.html', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=['POST'])
+def update_user(user_id):
+    user = User.query.get(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    """Delete the user."""
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect('/users')
+
--- 28 SQLAlchemy/28.1 Intro/models.py
+++ 28 SQLAlchemy/28.1 Intro/models.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -14,7 +16,11 @@ class User(db.Model):
                     primary_key=True,
                     autoincrement=True)
     first_name = db.Column(db.String(50),
-                    nullable=False)
+                    nullable=False,)
     last_name = db.Column(db.String(50),
                     nullable=False)
-    image_url = db.Column(db.String())
+    image_url = db.Column(db.String(10000))
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
