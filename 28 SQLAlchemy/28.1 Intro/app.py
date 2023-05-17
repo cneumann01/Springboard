e0abe760a6ed88e8c1ab62b2b78df7f6a2c158e0
cneumann01 <carterneumann@protonmail.com>
2023-05-17 09:59:00 -0400

    added flask template/user model

--- 28 SQLAlchemy/28.1 Intro/app.py
+++ 28 SQLAlchemy/28.1 Intro/app.py
@@ -1,5 +1,4 @@
 """Blogly application."""
-
 from flask import Flask
 from models import db, connect_db
 
@@ -7,6 +6,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
-db.create_all()
+# db.create_all()
