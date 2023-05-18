6e5a4ddb34d112bc95772caec8168c76a4f56ce2
cneumann01 <carterneumann@protonmail.com>
2023-05-18 17:30:48 -0400

    basic post framework

--- 28 SQLAlchemy/28.1 Intro/app.py
+++ 28 SQLAlchemy/28.1 Intro/app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
-from flask import Flask, redirect, render_template, request, flash, session
-from models import db, connect_db, User
+from flask import Flask, redirect, render_template, request, flash
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -13,10 +13,12 @@ connect_db(app)
 with app.app_context():
     db.create_all()
 
+
 @app.route('/')
 def home():
     """Redirect to list of users."""
     return redirect('/users')
+
 
 @app.route('/users')
 def list_users():
@@ -24,10 +26,12 @@ def list_users():
     users = User.query.all()
     return render_template('list.html', users=users)
 
+
 @app.route('/users/new')
 def new_user_form():
     """Show an add form for users."""
     return render_template('new.html')
+
 
 @app.route('/users/new', methods=['POST'])
 def add_user():
@@ -35,11 +39,13 @@ def add_user():
     last_name = request.form['last_name']
     image_url = request.form['image_url']
 
-    new_user = User(first_name=first_name,last_name=last_name,image_url=image_url)
+    new_user = User(first_name=first_name,
+                    last_name=last_name, image_url=image_url)
     db.session.add(new_user)
     db.session.commit()
 
     return redirect('/users')
+
 
 @app.route('/users/<int:user_id>')
 def show_user(user_id):
@@ -47,11 +53,13 @@ def show_user(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('show.html', user=user)
 
+
 @app.route('/users/<int:user_id>/edit')
 def edit_user(user_id):
     """Show edit form for a user."""
     user = User.query.get_or_404(user_id)
     return render_template('edit.html', user=user)
+
 
 @app.route('/users/<int:user_id>/edit', methods=['POST'])
 def update_user(user_id):
@@ -63,6 +71,7 @@ def update_user(user_id):
 
     return redirect('/users')
 
+
 @app.route('/users/<int:user_id>/delete', methods=['POST'])
 def delete_user(user_id):
     """Delete the user."""
@@ -71,3 +80,55 @@ def delete_user(user_id):
     db.session.commit()
     return redirect('/users')
 
+
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+@app.route('/users/<int:user_id>/posts/new')
+def new_post_form(user_id):
+    """Show an add form for posts."""
+    user = User.query.get_or_404(user_id)
+    return render_template('new_post.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def add_post(user_id):
+    title = request.form['title']
+    content = request.form['content']
+
+    new_post = Post(title=title, content=content, user_id=user_id)
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect('/users')
+
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    """Show a post."""
+    post = Post.query.get_or_404(post_id)
+    return render_template('show_post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    """Show an edit form for posts."""
+    post = Post.query.get_or_404(post_id)
+    return render_template('edit_post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def update_post(post_id):
+    """Update a post."""
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+    db.session.commit()
+    return redirect(f'/posts/{post_id}')
+
+
+@app.route('/posts/<int:post_id>/delete')
+def delete_post(post_id):
+    """Delete the post."""
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/users')
--- 28 SQLAlchemy/28.1 Intro/models.py
+++ 28 SQLAlchemy/28.1 Intro/models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -13,14 +14,31 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,
-                    primary_key=True,
-                    autoincrement=True)
+                primary_key=True,
+                autoincrement=True)
     first_name = db.Column(db.String(50),
-                    nullable=False,)
+                nullable=False,)
     last_name = db.Column(db.String(50),
-                    nullable=False)
+                nullable=False)
     image_url = db.Column(db.String(10000))
 
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Post(db.Column):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                primary_key=True,
+                autoincrement=True)
+    title = db.Column(db.String(50),
+                nullable=False,)
+    content = db.Column(db.String(10000),
+                nullable=False)
+    created_at = db.Column(db.DateTime,
+                nullable=False,
+                default=datetime.datetime.now)
+    user_id = db.Column(db.Integer,
+                db.ForeignKey('User.id'))
