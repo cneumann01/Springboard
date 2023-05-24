aad9a0eb920891d8582918f17ca66454fd220c70
cneumann01 <carterneumann@protonmail.com>
2023-05-24 13:41:25 -0400

    added models for tags/post-tag relationship

--- 28 SQLAlchemy/28.1 Intro/models.py
+++ 28 SQLAlchemy/28.1 Intro/models.py
@@ -1,6 +1,8 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.types import Integer
 import datetime
 
 db = SQLAlchemy()
@@ -33,13 +35,33 @@ class Post(db.Model):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    title = db.Column(db.String(50),
+                    nullable=False,)
+    content = db.Column(db.String(10000),
+                    nullable=False)
+    created_at = db.Column(db.DateTime,
+                    default=datetime.datetime.now)
+    user_id = db.Column(db.Integer,
+                    db.ForeignKey('users.id', ondelete='CASCADE'))
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer,
                 primary_key=True,
                 autoincrement=True)
-    title = db.Column(db.String(50),
-                nullable=False,)
-    content = db.Column(db.String(10000),
+    name = db.Column(db.String(50),
                 nullable=False)
-    created_at = db.Column(db.DateTime,
-                default=datetime.datetime.now)
-    user_id = db.Column(db.Integer,
-                db.ForeignKey('users.id', ondelete='CASCADE'))
+    posts = relationship('Post', secondary='post_tags', backref='tags')
+
+
+class PostTag(db.Model):
+    __tablename__ = 'post_tags'
+
+    post_id = db.Column(Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(Integer, db.ForeignKey('tags.id'), primary_key=True)
+    post = relationship('Post')
+    tag = relationship('Tag')
