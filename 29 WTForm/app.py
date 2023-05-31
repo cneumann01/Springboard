cdf61df88c841b6aa257723b74d61705932c8735
cneumann01 <carterneumann@protonmail.com>
2023-05-31 11:28:11 -0400

    edit pet/pet details

--- 29 WTForm/app.py
+++ 29 WTForm/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from forms import AddPetForm
+from forms import AddPetForm, EditPetForm
 from models import db, connect_db, Pet
 
 app = Flask(__name__)
@@ -17,15 +17,33 @@ with app.app_context():
 
 @app.route('/')
 def list_pets():
+    """List all pets."""
     pets = Pet.query.all()
     return render_template("list_pets.html", pets=pets)
 
 
 @app.route('/add', methods=["GET", "POST"])
 def add_pet():
+    """Show add-pet form and handle form submission."""
     form = AddPetForm()
     if form.validate_on_submit():
+        db.session.add(Pet(name=form.name.data, species=form.species.data, photo_url=form.photo_url.data, age=form.age.data, notes=form.notes.data, available=form.available.data))
+        db.session.commit()
         return redirect('/')
     else:
         return render_template("add_pet_form.html", form=form)
+    
+@app.route('/<int:pet_id>', methods=["GET", "POST"])
+def pet_details(pet_id):
+    """List details about a single pet."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.commit()
+    return render_template("pet_details.html", pet=pet, form=form)
+
     
--- 29 WTForm/forms.py
+++ 29 WTForm/forms.py
@@ -1,10 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+from wtforms import StringField, IntegerField, BooleanField, SelectField, validators
 
 
 class AddPetForm(FlaskForm):
-    name = StringField("Pet Name")
-    species = StringField("Species")
-    photo_url = StringField("Photo URL")
-    age = FloatField("Age")
-    notes = StringField("Notes")
+    """Form for adding pets."""
+    name = StringField('Pet Name', validators=[validators.InputRequired()])
+    species = SelectField("Species", choices=[('Dog', 'Dog'), ('Cat', 'Cat'), ('Bird', 'Bird')])
+    photo_url = StringField('Photo URL', validators=[
+                    validators.URL(), validators.Optional()])
+    age = IntegerField('Age', validators=[
+                    validators.NumberRange(min=0, max=30)])
+    notes = StringField('Notes', validators=[validators.Optional()])
+    available = BooleanField('Available', validators=[validators.Optional()])
+
+class EditPetForm(FlaskForm):
+    """Form for editing pets."""
+    photo_url = StringField('Photo URL', validators=[
+                    validators.URL(), validators.Optional()])
+    notes = StringField('Notes', validators=[validators.Optional()])
+    available = BooleanField('Available', validators=[validators.Optional()])
+
+
+
